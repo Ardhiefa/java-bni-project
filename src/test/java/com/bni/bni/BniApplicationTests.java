fa7e379aef9f6ac4c995b6773f411a4a119eb128
Ardhiefa <ardhiefa@gmail.com>
2025-06-10 08:47:36 +0700

    coba push

--- src/test/java/com/bni/bni/BniApplicationTests.java
+++ src/test/java/com/bni/bni/BniApplicationTests.java
@@ -2,7 +2,7 @@ package com.bni.bni;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//coba untuk push
 @SpringBootTest
 class BniApplicationTests {
 
